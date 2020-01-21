a4f650e50da6d37884609315f0090de5cf54bc3d
Quentin Laporte-Chabasse <quentin.laporte-chabasse@inria.fr>
2020-01-21 12:55:27 +0100

    [U] .gitignore

---
+++ ABC_Shadow_cpp/ConfigReader.cpp
@@ -0,0 +1,9 @@
+//
+//  ConfigReader.cpp
+//  ABC_Shadow_cpp
+//
+//  Created by Quentin on 15/01/2020.
+//  Copyright © 2020 Quentin. All rights reserved.
+//
+
+#include "ConfigReader.hpp"
---
+++ ABC_Shadow_cpp/ConfigReader.hpp
@@ -0,0 +1,14 @@
+//
+//  ConfigReader.hpp
+//  ABC_Shadow_cpp
+//
+//  Created by Quentin on 15/01/2020.
+//  Copyright © 2020 Quentin. All rights reserved.
+//
+
+#ifndef ConfigReader_hpp
+#define ConfigReader_hpp
+
+#include <stdio.h>
+
+#endif /* ConfigReader_hpp */
